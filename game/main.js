8b9d815020a7b522328e1c576604c13bd4a2178c
popesp <shawncsutherland@gmail.com>
2022-03-31 11:35:01 -0600

    Initial commit

---
+++ game/main.js
@@ -0,0 +1,38 @@
+const WIDTH_CANVAS = 800;
+const HEIGHT_CANVAS = 600;
+
+
+document.addEventListener("DOMContentLoaded", function()
+{
+	const dom_container = document.getElementById("container");
+
+	const game = new Phaser.Game({
+		type: Phaser.AUTO,
+		title: "LD48",
+		parent: dom_container,
+		width: WIDTH_CANVAS/2,
+		height: HEIGHT_CANVAS/2,
+		resolution: 5,
+		backgroundColor: 0x0a0808
+	});
+
+	function resize()
+	{
+		let w = window.innerWidth;
+		let h = window.innerHeight;
+
+		const r = HEIGHT_CANVAS/WIDTH_CANVAS;
+
+		if(w*r > window.innerHeight)
+			w = Math.min(w, Math.ceil(h/r), WIDTH_CANVAS);
+		h = Math.floor(w*r);
+
+		dom_container.style.width = game.canvas.style.width = `${w}px`;
+		dom_container.style.height = game.canvas.style.height = `${h}px`;
+		dom_container.style.top = `${Math.floor((window.innerHeight - h)/2)}px`;
+		dom_container.style.left = `${Math.floor((window.innerWidth - w)/2)}px`;
+	}
+
+	window.addEventListener("resize", resize);
+	resize();
+});
